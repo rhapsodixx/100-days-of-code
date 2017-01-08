a276d789ac9dfd49f892858341c1204a5ef07830
panji gautama <panji.gautama@gmail.com>
2017-01-08 10:18:54 +0700

    Refactor jsx files to js since it cannot be read by babel

---
+++ src/react-router-express/src/app-client.js
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from './components/AppRoutes';
+
+window.onload = () => {
+  ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
+};
